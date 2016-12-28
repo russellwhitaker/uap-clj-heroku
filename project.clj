5d8ea52126e6352b121256072db20fa9b67c1d66
Russell Whitaker <rwhitaker@ulive.com>
2016-12-27 21:15:18 -0800

    Workaround for lein-env issue

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
             [lein-ancient         "0.6.10"]
             [jonase/eastwood      "0.2.3"]
             [speclj               "3.3.2"]]
-  :hooks [environ.leiningen.hooks]
+  ;:hooks [environ.leiningen.hooks]
   :uberjar-name "uap-clj-heroku-standalone.jar"
   :profiles {:production {:env {:production true}}
              :uberjar {:aot :all}
