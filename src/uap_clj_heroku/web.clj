a7f8d9b182ff495558104e1eaa42258a68aa5d63
Russell Whitaker <rwhitaker@ulive.com>
2016-12-28 00:07:44 -0800

    foo

--- src/uap_clj_heroku/web.clj
+++ src/uap_clj_heroku/web.clj
@@ -26,7 +26,7 @@
   (ANY "/useragent" req
        {:status 200
         :headers {"Content-Type" "application/json"}
-        :body (pr-str (useragent (get-in req [:params "useragent"] "")))})
+        :body (pr-str (useragent (get-in req [:params "useragent"] (pr-str req))))})
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
 
